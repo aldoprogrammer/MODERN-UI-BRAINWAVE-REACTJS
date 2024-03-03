98218e4b9b0e6ed17f39969b196a3b07a6c281b0
aldoprogrammer <aldobesma@gmail.com>
2024-03-03 15:17:07 +0700

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { brainwave } from '../assets'
+import { navigation } from '../constants'
+import { useLocation } from 'react-router-dom'
+const Header = () => {
+  return (
+    <div className='fixed top-0 z-50 bg-n-8/90
+    backdrop-blur-sm border-b border-n-6
+    lg:bg-n-8/90 lg:backdrop-blur-sm'>
+        <div className="flex items-center
+        px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+            <a href="#hero" className='block 
+            w-[12rem] xl:mr-8'>
+            <img src={brainwave} alt="" 
+                width={190} height={40}
+            />
+            </a>
+
+            <nav className='hidden lg:flex fixed top-[5rem]
+            left-0 right-0 bottom-0 bg-n-8 lg:static
+            lg:mx-auto lg:bg-transparent'>
+                <div className="relative z-2 flex flex-col
+                items-center justify-center m-auto
+                lg:flex-row">
+                    {navigation.map((item) => (
+                        <a href={item.url} key={item.id}
+                            className={`block relative font-bode text-2xl
+                            uppercase text-n-1 transition-color 
+                            hover:text-color-1 ${item.onlyMobile 
+                            ? "lg:hidden" 
+                            : ""} px-6 py-6 md:py-8 lg:font-semibold
+                            lg:text-xl lg:-mr-0.25`}
+                        >
+                            {item.title}
+                        </a>
+                    ))}
+                </div>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
