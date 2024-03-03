179443a983efdd60dc8d2f081437016f629c13ec
aldoprogrammer <aldobesma@gmail.com>
2024-03-03 13:57:37 +0700

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,26 @@ const Button = ({ className, href, onClick, children, px, white}) => {
     h-11 transition-colors hover:text-color-1 ${px || 'px-7'}
     ${white ? 'text-n-8' : 'text-n-1'} ${className || ''} `;
   
+    const spanClasses = 'relative z-10'
     const renderButton = () => (
     <button className={classes}>
-        <span>{children}</span>
+        <span className={spanClasses}>{children}</span>
         {ButtonSvg(white)}
     </button>
   )
 
-  return renderButton();
+  const renderLink = () => (
+    <a href={href} className={classes}>
+        <span className={spanClasses}>
+            {children}
+            {ButtonSvg(white)}
+        </span>
+    </a>
+  )
+
+  return href
+  ? renderLink()
+  : renderButton();
 
 
 }
